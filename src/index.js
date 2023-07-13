c3c3c7f022a1df975893e1a953a0cc65dc808f8b
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-13 17:35:25 +0300

    <TaskLister Mini-Project>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+form.addEventListener("submit", function(event) {
+  // Prevent the default behavior of the form submit event.
+  event.preventDefault();
+
+  // Get the task description from the input field.
+  const taskDescription = document.getElementById("new-task-description").value;
+
+  // Add the task to the DOM.
+  const tasksList = document.getElementById("tasks");
+  const listItem = document.createElement("li");
+  listItem.textContent = taskDescription;
+  tasksList.appendChild(listItem);
+});
 });
